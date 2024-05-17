81c691d2ec250d6f26e9f49faad2e98131242c0a
martintcuft <mtorrescortez@uft.edu>
2024-05-17 15:12:24 -0400

    Música y movimiento de cámara

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public Transform toFollow;//el cuerpo a seguir
+	public Camera cam;
+    void Start()
+    {
+        toFollow = GameObject.Find("Epi").transform;
+    }
+    void Update()
+	{
+        if(toFollow != null) {//si el cuerpo no es nulo
+			//posición vertical respecto al jugador
+			float ytrans = toFollow.position.y + 1.5f;
+			//desde -1.0 hasta 3.5
+			if(ytrans > 3.5f) ytrans = 3.5f;
+			if(ytrans < -1f) ytrans = -1f;
+			
+			if(ytrans > -1.5f) {
+				cam.orthographicSize = 5+((ytrans+1f)/9f);//desde 5.0 hasta 5.5
+			}
+			else cam.orthographicSize = 5.0f;
+			
+			transform.position = new Vector3(transform.position.x, transform.position.y + (ytrans-transform.position.y)*Time.deltaTime, transform.position.z);
+		}
+    }
+}
